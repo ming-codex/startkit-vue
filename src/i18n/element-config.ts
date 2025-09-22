a140953d82c95824c859e77ea6c5f5f049f5d771
chenhuaming <364617737@qq.com>
2025-09-22 12:03:19 +0800

    国际化优化

--- src/i18n/element-config.ts
+++ src/i18n/element-config.ts
@@ -1,15 +1,21 @@
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import type { Language } from 'element-plus/es/locale'
+import elZh from 'element-plus/es/locale/lang/zh-cn'
+import elEn from 'element-plus/es/locale/lang/en'
 
 export function useElementConfig() {
-  const { locale, messages } = useI18n()
+  const { locale } = useI18n()
 
-  const elementConfig = computed(() => ({
-    locale: messages.value[locale.value].el as any as Language,
-    size: 'small' as const,
-    zIndex: 3000,
-  }))
+  const elementConfig = computed(() => {
+    const elLocale = locale.value === 'zh' ? elZh : elEn
+
+    return {
+      locale: elLocale as Language,
+      size: 'small' as const,
+      zIndex: 3000,
+    }
+  })
 
   return {
     elementConfig,
